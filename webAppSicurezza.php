da30c6ca9491389495579a860adb5c7a4ab15086
Michele Barzan <michele@servizioglobale.it>
2020-03-13 09:53:21 +0100

    webappsicurezza location invece che open

--- webAppSicurezza.php
+++ webAppSicurezza.php
@@ -1,1 +1,1 @@
-<script>window.open("https://nordpas.q81hsewebapp.com/Login.aspx?ReturnUrl=%2f");window.location = "index.php";</script>
+<script>window.location="https://nordpas.q81hsewebapp.com/Login.aspx?ReturnUrl=%2f";</script>
