6850a99bb68a92f5b0c279b59a0c5b4217528dff
Michele Barzan <michele@servizioglobale.it>
2020-03-16 16:48:59 +0100

    foto ordini ok

--- getFotoImmagini.php
+++ getFotoImmagini.php
@@ -10,7 +10,7 @@
 	$server_adress=$_SERVER['SERVER_ADDR'];
 	$path="http://$server_adress/oasisfotoproduzione/";
 
-	$query2="SELECT DISTINCT dbo.registrazioni_produzione.ordine, dbo.registrazioni_produzione.stazione, REPLACE(REPLACE(dbo.allegati_registrazioni_produzione.percorso, '+', ''), '//srv-01/Produzione/', '') AS percorso FROM dbo.allegati_registrazioni_produzione INNER JOIN dbo.registrazioni_produzione ON dbo.allegati_registrazioni_produzione.registrazione_produzione = dbo.registrazioni_produzione.id_registrazione WHERE ordine='$ordine' AND stazione='$stazione'";	
+	$query2="SELECT DISTINCT dbo.registrazioni_produzione.ordine, dbo.registrazioni_produzione.stazione, REPLACE(REPLACE(dbo.allegati_registrazioni_produzione.percorso, '+', ''), '//srv-dati/Produzione/', '') AS percorso FROM dbo.allegati_registrazioni_produzione INNER JOIN dbo.registrazioni_produzione ON dbo.allegati_registrazioni_produzione.registrazione_produzione = dbo.registrazioni_produzione.id_registrazione WHERE ordine='$ordine' AND stazione='$stazione'";	
 	$result2=sqlsrv_query($conn,$query2);
 	if($result2==FALSE)
 	{
