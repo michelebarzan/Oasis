1bbb97fa8e1d9b58fc18b3bce5bc68206da3f09a
michele barzan <michele@servizioglobale.it>
2022-08-22 14:59:26 +0200

    ok

--- importaPdf.php
+++ importaPdf.php
@@ -4,6 +4,8 @@
 		
 	if(set_time_limit(120))
 	{
+		$output2 = shell_exec("net use \\\\srv-cont\\K1OasisCatalogo\\Ordini\\Produzione /delete 2>&1");
+		$output2 = shell_exec("net use \\\\srv-cont\\K1OasisCatalogo\\Ordini\\Produzione Toiurech01. /user:quaia-oasis\loris 2>&1");
 		$output3 = shell_exec("robocopy \\\\srv-cont\\K1OasisCatalogo\\Ordini\\Produzione C:\\xampp\\htdocs\\OasisPdfOrdini\\pdf_ordini *.pdf /z 2>&1");
 		$output4 = shell_exec("robocopy \\\\srv-cont\\K1OasisCatalogo\\Ordini C:\\xampp\\htdocs\\OasisPdfOrdini\\ordini_acquisto *.pdf /z 2>&1");
 		
